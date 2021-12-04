c98d9d17285c994c5f1cee7a39c505189c2944a2
Kathleen <88250122+cult4nthropo@users.noreply.github.com>
2021-12-04 22:21:41 +0100

    first commit

---
+++ src/fileSizeCalculator/Scan.java
@@ -0,0 +1,19 @@
+/**
+ * @author Kathleen Prasatko
+ * 
+ * Calculates the file size of a scan
+ * general formula: ((width * height) * resolution) / compression
+ * to get the right file size one has to beware of the measurement units like bit and byte or inch and cm
+ * as well as the unit of the wanted result
+ */
+
+package fileSizeCalculator;
+
+class Scan {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/fileSizeCalculator/Sound.java
@@ -0,0 +1,10 @@
+package fileSizeCalculator;
+
+class Sound {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

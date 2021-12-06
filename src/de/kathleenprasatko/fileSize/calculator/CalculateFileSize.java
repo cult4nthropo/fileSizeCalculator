b9f1284cd7c81f10e5d47f2409668baa185fc9ae
cult4nthropo <88250122+cult4nthropo@users.noreply.github.com>
2021-12-06 15:59:00 +0100

    corrected scan

--- src/de/kathleenprasatko/fileSize/calculator/CalculateFileSize.java
+++ src/de/kathleenprasatko/fileSize/calculator/CalculateFileSize.java
@@ -7,40 +7,42 @@ import de.kathleenprasatko.fileSize.Videofile;
 public class CalculateFileSize {
 
 	public static void main(String[] args) {
+		//To-Do: let the user choose the task
+		
 		Scan scan = new Scan();
-		scan.setWidth(10.5);
-		scan.setHeight(12.0);
-		scan.setDepth(32);
-		scan.setResolution(760);
-		scan.setCompression(8);
-		
-		//TO-DO: set those values by user input
-		
-		double fileSizeScan = scan.calculateFileSize(scan);
-		
-		System.out.println(fileSizeScan);
-		
+			scan.setWidth(10.5);
+			scan.setHeight(12.0);
+			scan.setDepth(32);
+			scan.setResolution(760);
+			scan.setCompression(8);
+			
+			//TO-DO: set those values by user input
+			
+			double fileSizeScan = scan.calculateFileSize(scan);
+			
+			System.out.println(fileSizeScan);
+			
 		Soundfile soundfile = new Soundfile();
-		soundfile.setChannels(2);
-		soundfile.setDuration(63);
-		soundfile.setFrequency(3600);
-		soundfile.setResolution(64);
-		
-		double fileSizeSound = soundfile.calculateFileSize(soundfile);
+			soundfile.setChannels(2);
+			soundfile.setDuration(63);
+			soundfile.setFrequency(3600);
+			soundfile.setResolution(64);
+			
+			double fileSizeSound = soundfile.calculateFileSize(soundfile);
 		
 		System.out.println(fileSizeSound);
 		
 		Videofile videofile = new Videofile();
-		videofile.setWidth(4500);
-		videofile.setHeight(3500);
-		videofile.setDepth(8);
-		videofile.setFramesPerSecond(65);
-		videofile.setDuration(125);
-		videofile.setCompression(64);
-		
-		double fileSizeVideo = videofile.calculateFileSize(videofile);
-		
-		System.out.println(fileSizeVideo);
+			videofile.setWidth(4500);
+			videofile.setHeight(3500);
+			videofile.setDepth(8);
+			videofile.setFramesPerSecond(65);
+			videofile.setDuration(125);
+			videofile.setCompression(64);
+			
+			double fileSizeVideo = videofile.calculateFileSize(videofile);
+			
+			System.out.println(fileSizeVideo);
 	}
 
 }
